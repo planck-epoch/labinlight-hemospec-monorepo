312b69fd7b11d76321250e29ca848f1540b360ff
William Sousa <william.sousa@augentic.com>
2025-12-10 04:59:54 +0000

    code refactor

--- hemospec-app/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ hemospec-app/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,11 +2,17 @@ package io.ionic.starter;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import java.io.IOException;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(MetaScanPlugin.class);
         super.onCreate(savedInstanceState);
+        try {
+            Runtime.getRuntime().exec("logcat -d");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
